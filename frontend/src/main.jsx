0484148c5a7d2e606d48c990671bf7a15f597454
Alxks4o <yonashkovaleks07@gmail.com>
2026-02-18 01:08:14 +0000

    1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -33,6 +33,10 @@ const router = createBrowserRouter(
     {
       path: '/photography',
       element: <Photography />
+    },
+    {
+      path: '*',
+      element: <Index />   
     }
   ],
   {
